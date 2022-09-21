16f7bceeb40cd4c5092c5ce16a28da451e5024b9
Beulah Mathenge <beulahmathenge@gmail.com>
2022-09-21 17:40:07 +0300

    Complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+  const [addItem, setAddItem]= useState(false)
+
+  function handleCart(){
+    setAddItem((addItem) => !addItem)
+  }
+
+  //const appClass = false ? "Remove From Cart" : "Add to Cart"
   return (
-    <li className="">
+    <li className={addItem? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={addItem ? "remove" : "add"} onClick={handleCart}>{addItem? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
 
 export default Item;
+
+// In the `Item` component, when the user clicks the `<button>` element, the item
+// should be added to their virtual cart. The way we'll show the user that the item
+// is in the cart is by changing the className on the `<li>` element:
